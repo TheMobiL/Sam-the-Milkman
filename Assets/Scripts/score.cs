b32b54a4b0a880a16dbabf28e72e35920cdb2820
S-vani <shayan.vanishoja86@gmail.com>
2024-09-18 17:51:33 -0400

    Small changes in movement and score

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+
+    public Text Score;
+
+    int score_int = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
