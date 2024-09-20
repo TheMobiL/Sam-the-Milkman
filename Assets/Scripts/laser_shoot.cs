4da57c0514f0c10cfdb3826db6ac0ddf67f20f84
TheMobiL <124905245+TheMobiL@users.noreply.github.com>
2024-09-20 14:18:46 -0400

    readded lasers and made them actually shoot
    right now the laser and enemy hitbox interaction is still broken (they're supposed to both disappear) but otherwise it works fine

---
+++ Assets/Scripts/laser_shoot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class laser_shoot : MonoBehaviour
+{
+    public float speed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (transform.position.x >= 4) //incredibly scuffed code that doesn't really work properly but this game is a throwaway so it's fine
+        {
+            Destroy(GameObject.Find("laser(Clone)"), 2);
+        }
+    }
+}
